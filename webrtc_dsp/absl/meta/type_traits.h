8682c5c22e9c3a28ee3aacfd1d529db07ea914bf
John Preston <johnprestonmail@gmail.com>
2020-05-21 11:23:58 +0400

    Fix build with MSVC c++latest.

--- webrtc_dsp/absl/meta/type_traits.h
+++ webrtc_dsp/absl/meta/type_traits.h
@@ -410,7 +410,7 @@ template <typename T>
 using underlying_type_t = typename std::underlying_type<T>::type;
 
 template <typename T>
-using result_of_t = typename std::result_of<T>::type;
+using result_of_t = typename std::invoke_result<T>::type;
 
 namespace type_traits_internal {
 template <typename Key, typename = size_t>
