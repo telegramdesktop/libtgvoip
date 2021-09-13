2607e1d63013d01e4c8662be15428017b5946ffc
Henry Hu <henry.hu.sh@gmail.com>
2021-09-13 16:57:56 -0700

    Add FreeBSD support.

--- VoIPController.cpp
+++ VoIPController.cpp
@@ -3005,7 +3005,7 @@ static void initMachTimestart() {
 #endif
 
 double VoIPController::GetCurrentTime(){
-#if defined(__linux__)
+#if defined(__linux__) || defined(__FreeBSD__)
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
 	return ts.tv_sec+(double)ts.tv_nsec/1000000000.0;
