82284c6d1cee3a655795fef26c58a61df5556e99
Gleb Smirnoff <glebius@FreeBSD.org>
2021-09-13 17:10:56 -0700

    Support pthread_setname_np() on FreeBSD and while here use positive logic in the ifdefs.

--- threading.h
+++ threading.h
@@ -18,6 +18,9 @@
 #ifdef __APPLE__
 #include "os/darwin/DarwinSpecific.h"
 #endif
+#ifdef __FreeBSD__
+#include <pthread_np.h>
+#endif
 
 namespace tgvoip{
 	class Mutex{
@@ -92,9 +95,9 @@ namespace tgvoip{
 		static void* ActualEntryPoint(void* arg){
 			Thread* self=reinterpret_cast<Thread*>(arg);
 			if(self->name){
-#if !defined(__APPLE__) && !defined(__gnu_hurd__)
+#if defined(__linux__) || defined(__FreeBSD__)
 				pthread_setname_np(self->thread, self->name);
-#elif !defined(__gnu_hurd__)
+#elif defined(__APPLE__)
 				pthread_setname_np(self->name);
 				if(self->maxPriority){
 					DarwinSpecific::SetCurrentThreadPriority(DarwinSpecific::THREAD_PRIO_USER_INTERACTIVE);
